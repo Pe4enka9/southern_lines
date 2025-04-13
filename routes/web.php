322fb2e491ff3df40ed0b853c941146320b34ab1
Ivan Tihonov <ivan.tixonov000@gmail.com>
2025-04-13 22:48:20 +0700

    Добавлен перфикс admin к маршрутам

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,16 @@ use App\Http\Middleware\AdminMiddleware;
 use App\Http\Middleware\GuestMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(GuestMiddleware::class)->group(function () {
-    Route::get('/login', [AdminController::class, 'loginForm'])->name('admin.login-form');
-    Route::post('/login', [AdminController::class, 'login'])->name('admin.login');
-});
+Route::prefix('admin')->group(function () {
+    Route::middleware(GuestMiddleware::class)->group(function () {
+        Route::get('/login', [AdminController::class, 'loginForm'])->name('admin.login-form');
+        Route::post('/login', [AdminController::class, 'login'])->name('admin.login');
+    });
 
-Route::middleware(AdminMiddleware::class)->group(function () {
-    Route::post('/logout', [AdminController::class, 'logout'])->name('admin.logout');
+    Route::middleware(AdminMiddleware::class)->group(function () {
+        Route::post('/logout', [AdminController::class, 'logout'])->name('admin.logout');
 
-    Route::get('/applications', [ApplicationController::class, 'index'])->name('admin.application.index');
-    Route::get('/applications/{application}', [ApplicationController::class, 'show'])->name('admin.application.show');
+        Route::get('/applications', [ApplicationController::class, 'index'])->name('admin.application.index');
+        Route::get('/applications/{application}', [ApplicationController::class, 'show'])->name('admin.application.show');
+    });
 });
